d3947645e362efc395adc5872ec647b33ed1bc69
Carlos Arias <733@holbertonschool.com>
2019-10-04 23:46:47 -0500

    Delete /script/main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-$.ajax({
-  type: 'GET',
-  dataType: 'json',
-  url: 'https://intranet.hbtn.io/users/me.json?auth_token=ef2aefa07313fca805ce9a90726e3263a8dd060213239333b716f1954e508875'
-}).done(function (data) {
-  console.log(data);
-});
